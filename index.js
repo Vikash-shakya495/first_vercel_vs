b722a9bb3b00d4291f05903b0783ce98918df117
Vikash-shakya495 <vikashshakya367@gmail.com>
2024-06-14 10:25:45 +0530

    Task 26

--- index.js
+++ index.js
@@ -5,19 +5,22 @@ const PORT = process.env.PORT || 3000; // port 80 is bydefault
 // const hostname = "localhost";
 
 const home = fs.readFileSync('./index.html','utf-8');
+const about = fs.readFileSync('./about.html','utf-8');
+const contact = fs.readFileSync('./contact.html','utf-8');
+const service = fs.readFileSync('./service.html','utf-8');
 
 const server = http.createServer((req,res)=>{
     if(req.url==="/"){
         return res.end(home);
     }
     if(req.url==="/about"){
-        return res.end("<h1>About Page</h1>");
+        return res.end(about);
     }
     if(req.url==="/contact"){
-        return res.end("<h1>Contact Page</h1>");
+        return res.end(contact);
     }
     if(req.url==="/service"){
-        return res.end("<h1>Service Page</h1>");
+        return res.end(service);
     }
     else{
         return res.end("<h1>404 Page Not Found</h1>");
